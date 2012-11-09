218efcb9c23df6c2eee215828e42917ea9ddc8da
Daniel Glauser <daniel@snugghome.com>
2012-11-08 18:38:32 -0700

    Fixed the lazyness issue, started adding clj-time

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-swank "1.4.4"]]
   :dependencies [[org.clojure/clojure "1.4.0"]
+                 [clj-time "0.4.4"]
                  [clojure-csv/clojure-csv "2.0.0-alpha1"]]
   :main energyanalyzer.core)
--- src/energyanalyzer/core.clj
+++ src/energyanalyzer/core.clj
@@ -1,6 +1,8 @@
 (ns energyanalyzer.core
   (:require [clojure-csv.core :as csv]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clj-time.core :as time]
+            [clj-time.format :as time-format]))
 
 (defn count-lines
   "Lazily counts the lines in a file."
@@ -12,18 +14,30 @@
 (defn parse-line
   "Parses a single line of CSV data into date, kWh, and qualityOfReading"
   [line]
-  line)
+  (str line "\n"))
 
 (defn parse-csv
   "Skips the first line of the CSV and parses the data with a given line-paser function."
   [line-parser filename]
   (with-open [rdr (clojure.java.io/reader filename)]
     (let [lazy-seq-of-lines (line-seq rdr)]
-      (map line-parser lazy-seq-of-lines))))
+      (doall (map line-parser lazy-seq-of-lines)))))
 ;; (parse-csv parse-line "data/Test15Minute.csv")
 
+(defn doto-csv
+  "Skips the first line of the CSV and parses the data with a given line-paser function."
+  [line-function filename]
+  (with-open [rdr (clojure.java.io/reader filename)]
+    (let [lazy-seq-of-lines (line-seq rdr)]
+      (map line-function lazy-seq-of-lines))))
+
 (defn -main []
-  (println "CSV Data: " (parse-csv "data/Test15Minute.csv")))
+  (println "CSV Data: " (parse-csv parse-line "data/Test15Minute.csv")))
+
+#_(defn -main []
+  (doto-csv parse-line "data/Test15Minute.csv"))
 
 ;; (-main)
 ;; (count-lines "data/Test15Minute.csv")
+;; (parse-csv parse-line "data/Test15Minute.csv")
+;; (with-open [rdr (clojure.java.io/reader "data/Test15Minute.csv")] )
